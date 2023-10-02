983448ffef450a46322f9a57d8adf6ecf7327520
Justin Hennessy <292017+justinhennessy@users.noreply.github.com>
2023-10-02 18:36:32 +1000

    Added logging

--- bot.py
+++ bot.py
@@ -6,13 +6,19 @@ from twitchio.user import User
 import requests
 import json
 from oauth import request_oauth_token
+import pprint
+import logging  # New import for logging
+
+# Configure logging
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    handlers=[logging.FileHandler('bot.log', encoding='utf-8'), logging.StreamHandler()])
+
+logger = logging.getLogger(__name__)
 
 class Bot(commands.Bot):
     
     def __init__(self):
-        # Initialise our Bot with our access token, prefix and a list of channels to join on boot...
-        # prefix can be a callable, which returns a list of strings or a string...
-        # initial_channels can also be a callable which returns a list of strings...
         load_dotenv()
         twitch_oauth_token = os.getenv("TWITCH_OAUTH_TOKEN")
         twitch_channel_name = os.getenv("TWITCH_CHANNEL_NAME")
@@ -21,29 +27,25 @@ class Bot(commands.Bot):
         super().__init__(token=twitch_oauth_token, prefix='?', initial_channels=[twitch_channel_name])
 
     async def event_ready(self):
-        # Notify us when everything is ready!
-        # We are logged in and ready to chat and use commands...
-        print(f'Logged in as | {self.nick}')
-        print(f'User id is | {self.user_id}')
+        logger.info(f'Logged in as | {self.nick}')
+        logger.info(f'User id is | {self.user_id}')
 
     async def event_message(self, message):
-        # Messages with echo set to True are messages sent by the bot...
-        # For now we just want to ignore them...
         if message.echo:
             return
 
-        data = {"username": self.nick}
+        data = {"username": message.author.name}
+        logger.info(f"Received message from {message.author.name}")
         
         response = requests.post(self.url, json=data)
         
         if response.status_code == 200:
-            print(f"Message count incremented for user: {self.nick}")
+            logger.info(f"Attempting to increment message count for user: {message.author.name}")
         else:
-            print(f"Error: {response.status_code} - {response.text}")
+            logger.error(f"Error: {response.status_code} - {response.text}")
             
         await self.handle_commands(message)
 
-from dotenv import load_dotenv
 load_dotenv()
 
 bot = Bot()
